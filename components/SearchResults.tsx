9606bb389c26e60ced95c3f45b684f7ae04e2b54
Rafael Pelegrini <arrobarafael@hotmail.com>
2021-07-05 16:12:51 -0300

    Comitando adição de função nova

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -7,9 +7,10 @@ interface SearchResultsProps{
     price: number;
     title: string;
   }>
+  onAddToWishList: (id: number) => void;
 }
 
-export function SearchResults({results} : SearchResultsProps){
+export function SearchResults({results, onAddToWishList} : SearchResultsProps){
 
   const totalPrice = useMemo(() => {
     return results.reduce((total, product) => {
@@ -23,7 +24,7 @@ export function SearchResults({results} : SearchResultsProps){
 
       {results.map(product => {
         return(
-          <ProductItem product = {product} key={product.id} />
+          <ProductItem product = {product} key={product.id} onAddToWishList={onAddToWishList} />
         )
       })}
     </div>
